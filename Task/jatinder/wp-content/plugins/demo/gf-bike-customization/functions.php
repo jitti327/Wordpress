98343cdbae01dc866308af9b5716b066f8d2a932
incredible <jitti327@gmail.com>
2019-01-01 18:31:13 +0530

    Adding New Backup for Bikes in Jatinder

---
+++ Task/jatinder/wp-content/plugins/demo/gf-bike-customization/functions.php
@@ -0,0 +1,374 @@
+<?php
+/*
+Plugin Name: GF Bikes Customization
+Description: GF Bikes Customization
+Author: Developer D
+Version: 1.0
+*/
+
+class gfBikesCustomization{
+
+	private $formId = 98;
+
+    public $stageVariable = [];
+
+	public function __construct(){
+
+	    ## Add admin menus
+  	    add_action( 'admin_menu', array( $this, 'admin_bike_menu_page' ) );
+		
+		## Modify Gravity form HTML before render
+		add_filter( 'gform_pre_render_'.$this->formId, array( $this, 'preRenderProccess' ) );
+
+		## Hide Gravity Form Label
+        add_filter( 'gform_enable_field_label_visibility_settings', '__return_true' );
+
+        ## Need to move in WP Enqueue script
+        add_action( 'wp_head', function(){
+            ?>  
+            <style>
+                .bikes-hidden-options ul.gfield_radio li{
+                    display:none !important;
+                }
+                ul.gfield_radio li.active-child{
+                    display:inline-block !important;
+                }
+            </style>
+            <script>
+                jQuery( document ).ready(function(){
+
+                	var changePrice = function( self ){
+                		var className     = self.parent().attr("class");
+                		var tempPrice     = className.split('data-price-');
+                		var price         = tempPrice[1];
+                		if( className.indexOf('active-child') > 1  ){
+                			tempPrice = price.split(' active-child');
+                			price = tempPrice[0];
+                		}
+                		jQuery('#field_98_155 input[type="text"]').val(price);
+                        jQuery('#input_98_155').trigger("change");
+                        //console.log(price);
+                	}
+                	var onClickProcess = function( self, level ){
+                		var elementId = self.val();
+                        jQuery('.bikes-hidden-options.level-'+level+' ul.gfield_radio li').removeClass('active-child');
+                        jQuery('.bikes-hidden-options.level-'+level+' ul.gfield_radio li.js-parent-id-'+elementId).addClass('active-child');
+                        changePrice( self );
+                	}
+
+                    jQuery('#field_98_149 input[type="radio"]').click(function(){
+                        onClickProcess( jQuery(this), 2 );
+                        onClickProcess( jQuery(this), 3 );
+                    });
+
+                    jQuery('#field_98_150 input[type="radio"]').click(function(){
+                     	onClickProcess( jQuery(this), 3 );
+                    });
+
+                    jQuery('#field_98_151 input[type="radio"]').click(function(){
+                     	changePrice( jQuery(this) );
+                    });
+                });
+            </script>
+            <?php
+        } );
+
+	}
+
+	public function debug($info){
+		echo "<pre>";
+			print_r($info);
+		echo "</pre>";
+		die;
+	}
+
+    ## Register a custom menu page.
+	public function admin_bike_menu_page() {
+	    add_menu_page(
+	        'Manage Bikes',                             // page_title
+	        'Manage Bikes',                             // menu_title
+	        'manage_options',                           // Capability
+	        'manage-bikes',                    			// Slug
+	        array( $this, 'bikes' ),                    // Calling bikes Function here
+	        'dashicons-cart',                     		// Used For Icon
+	        4
+	    );
+
+	    add_submenu_page(                    
+	        'manage-bikes',                    			// url
+	        'Manage Ski',                    			// title name
+	        'Manage Ski',                             	// shortcode reference
+	        'manage_options',                           // Capability (who can use this option)
+	        'manage-ski',                       		// slug (unique of key)
+	        array( $this, 'ski' )      					// function(call back)
+	    );
+
+	    add_submenu_page(                    
+	        'manage-bikes',                    			// url
+	        'Manage Paddle',                    		// title name
+	        'Manage Paddle',                            // shortcode reference
+	        'manage_options',                           // Capability (who can use this option)
+	        'manage-paddle',                       		// slug (unique of key)
+	        array( $this, 'Paddle' )      				// function(call back)
+	    );
+	}
+
+	public function bikes(){
+	  	global $wpdb;
+
+	    if($_REQUEST['action'] == 'add'){
+	      include_once( plugin_dir_path( __FILE__ ).'admin/code/add.php');
+	      include_once( plugin_dir_path( __FILE__ ).'admin/add.php'); 
+	      return;
+	    }
+	    if(!isset($_REQUEST['action'])){
+	      include_once( plugin_dir_path( __FILE__ ).'admin/code/listing.php');
+	      include_once( plugin_dir_path( __FILE__ ).'admin/listing.php');
+	      return;
+	     }
+	    if($_REQUEST['action'] == 'deleted'){
+	      include_once( plugin_dir_path( __FILE__ ).'admin/code/listing.php');
+	      include_once( plugin_dir_path( __FILE__ ).'admin/listing.php');
+	      return;
+	     }
+	    if($_REQUEST['action'] == 'edit'){
+	      include_once( plugin_dir_path( __FILE__ ).'admin/code/edit.php');
+	      include_once( plugin_dir_path( __FILE__ ).'admin/edit.php');
+	      return;
+	    }
+	}
+
+	public function ski(){
+	  	global $wpdb;
+
+	    if($_REQUEST['action'] == 'add'){
+	      include_once( plugin_dir_path( __FILE__ ).'ski/code/add.php');
+	      include_once( plugin_dir_path( __FILE__ ).'ski/add.php'); 
+	      return;
+	    }
+	    if(!isset($_REQUEST['action'])){
+	      include_once( plugin_dir_path( __FILE__ ).'ski/code/listing.php');
+	      include_once( plugin_dir_path( __FILE__ ).'ski/listing.php');
+	      return;
+	     }
+	    if($_REQUEST['action'] == 'deleted'){
+	      include_once( plugin_dir_path( __FILE__ ).'ski/code/listing.php');
+	      include_once( plugin_dir_path( __FILE__ ).'ski/listing.php');
+	      return;
+	     }
+	    if($_REQUEST['action'] == 'edit'){
+	      include_once( plugin_dir_path( __FILE__ ).'ski/code/edit.php');
+	      include_once( plugin_dir_path( __FILE__ ).'ski/edit.php');
+	      return;
+	    }
+	}
+
+	public function Paddle(){
+	  	global $wpdb;	    
+	} 
+
+	##  Function Name : requiredMessage()
+	##  Parameter     : $status -> Enter the wordpress classs name for updated / error.
+	##  			  : $text -> Display your text 
+	##  Return        : $message                
+	public function requiredMessage($status , $text){
+	    $message = "<div class='".$status." notice'><p>".$text."</p></div>";
+	    return $message;
+	}
+
+	####
+	public function generalAddField($name , $displayName ,$value , $placeholder){ ?>
+	    <tr>
+	      <th scope="row"><label for="blogname"><?php echo $displayName; ?></label></th>
+	      <td>
+	        <input name="<?php echo $name; ?>" type="text" id="blogname" value="<?php echo $value; ?>" class="regular-text custom_text" placeholder="<?php echo $placeholder; ?>">
+	      </td>
+	    </tr>
+	<?php  }
+
+	####
+	public function generalAddtextField($name , $displayName ,$value , $placeholder){ ?>
+	    <tr>
+	      <th scope="row"><label for="blogname"><?php echo $displayName; ?></label></th>
+	      <td>
+	        <textarea name="<?php echo $name; ?>" type="text" id="blogname" class="regular-text custom_textarea" placeholder="<?php echo $placeholder; ?>"><?php echo $value; ?></textarea>
+	      </td>
+	    </tr>
+	<?php  }
+
+	## Function Name 	: generalbutton
+	##	Parameters 		: $name , $value
+	## 					: $name-> name of the button
+	##					: $value-> value for button for example save , add , register etc.
+	public function generalbutton($name ,$value){ ?>
+	    <p class="submit">
+	      <input type="submit" name="<?php echo $name; ?>" id="submit" class="button button-primary" value="<?php echo $value  ?>">
+	    </p>
+	<?php  }
+
+	####
+	public function generalcheckboxfields( $display , $name , $value , $id ){  ?>
+	    <tr class="option-site-visibility">
+	      <th scope="row"> Categories :</th>
+				<td>
+					<fieldset>	
+					<label for="<?php echo $name; ?>">
+						<input name="<?php echo $name; ?>" type="checkbox" id="<?php echo $id; ?>" value="<?php echo $value; ?>">
+							<?php echo $display; ?>
+					</label>
+				</fieldset>
+			</td>
+		</tr>
+	<?php }
+
+	####
+	public function generalLables($name , $type , $id , $value , $display){ 
+	    ?>
+	    <label for="<?php echo $name; ?>">
+	      <input type="<?php echo $type;?>" name="<?php echo $name; ?>" id="<?php echo $id; ?>" value="<?php echo $value; ?>"><?php echo $display; ?>
+	    </label>
+	<?php  }
+
+	####
+	public function priceField($name , $lableid , $type , $id , $value , $placeholder){ 
+	    if($type == 'number'){
+	      $type = 'text';
+	    } 
+	    ?>
+	    <label for="<?php echo $name; ?>" id="<?php echo $lableid; ?>" style="display: none;">
+	      <input name="<?php echo $name; ?>" type="<?php echo $type; ?>" id="<?php echo $id; ?>" value="<?php echo $value; ?>" class="small-text" placeholder="<?php echo $placeholder; ?>">
+	    </label>
+	<?php  }
+
+
+
+	###############################################
+	###############################################
+	###############################################
+
+	## Search for records in DB
+	public function getRecords( $parentId = 0 ){
+	
+		global $wpdb;
+		$table = $wpdb->prefix.'bike';
+
+		$frequency = array(
+							 "half-day"  => "Half day", 
+							 "full-day"  => "Full day" ,
+							 "day"       => "Day"
+						);
+
+		if( isset($_GET['vid']) ){
+			$matchVenoer = " AND vendor_id=".$_GET['vid'];
+		}
+
+		if( isset($_GET['fid']) ){
+			$matchFrequency = " AND frequency='".$frequency[$_GET['fid']]."'";
+		}
+
+		$bikeArray = $wpdb->get_results( "SELECT * FROM {$table} where value = 1 AND parent_id = {$parentId}{$matchVenoer}{$matchFrequency}" );
+
+		if( empty($bikeArray) ){
+			return false;
+		}
+
+		return $bikeArray;
+	}
+
+	###
+	public function getIterativeData( $records, $index = 0 ){
+
+        if(empty($this->stageVariable[ $index ])){
+          $this->stageVariable[ $index ] = [];
+        }
+
+        if( empty( $records ) ){
+        	return array();
+		}
+
+		foreach( $records as $bike ){
+			$this->stageVariable[$index][] =  array(
+									'id'         => $bike->id,
+									'name'       => $bike->name,
+									'frequency'  => $bike->frequency,
+									'price'		 => $bike->price,
+									'parent_id'  => $bike->parent_id,
+								  );
+			
+			$children = $this->getRecords( $bike->id );
+
+			if( !empty($children) ){
+				$newIndex = $index + 1;
+			    $this->getIterativeData( $children, $newIndex );
+			} 
+		}
+
+		return $temp;
+	}
+
+	###
+	public function getDropdown(  $feildId, $data = array() ) {
+
+		if( empty($data) ){
+			return false;
+		}
+
+        
+        $choices = [];
+		foreach( $data as $key => $value ) {
+
+            $choicesLength = count($choices);
+
+			#####
+			add_filter( 'gform_field_choice_markup_pre_render_'.$this->formId.'_'.$feildId, function ( $choice_markup, $choice, $field, $fvalue ) use ($key, $value, $choicesLength) {
+
+                $formId  = $this->formId;
+                $fieldId = $field->id;
+
+                $searchName = "gchoice_{$formId}_{$fieldId}_{$choicesLength}";
+
+                return $choice_markup = str_replace( $searchName,"{$searchName} js-parent-id-{$value['parent_id']} js-id-{$value['id']} data-price-{$value['price']}", $choice_markup );
+
+			}, 10, 4 );
+
+			####
+			$choices[] = array( 'text' => '<div class="ids-box"><img src="http://gf.incredible-developers.com/wp-content/uploads/2018/11/small-icon.png"><span class="gf-title">'.$value['name'].'</span></div>', 'value' => $value['id'] );
+		}
+		return $choices;
+
+	}
+
+	###
+	public function preRenderProccess($form){
+
+		$bikeArray = $this->getRecords();
+		$this->getIterativeData( $bikeArray );
+        $options   = $this->stageVariable;
+
+		foreach( $form['fields'] as &$field )  {
+
+			$choices = array();
+			$feildId = $field['id'];
+
+			switch( $feildId ){
+				case 149:
+					$field->choices = $this->getDropdown( $feildId, $options[0] ); 
+				break;
+				case 150:
+					$field->choices = $this->getDropdown( $feildId, $options[1] ); 
+				break;
+				case 151:
+					$field->choices = $this->getDropdown( $feildId, $options[2] ); 
+				break;
+			}
+
+		}
+
+		return $form;
+	}
+
+}
+
+add_action( 'plugins_loaded', function() {
+    new gfBikesCustomization();
+});
---
+++ Task/jatinder/wp-content/plugins/gf-bike-customization/admin/add_edit.php
@@ -0,0 +1,564 @@
+<?php
+  
+  $GLOBALS['edit'] = ($_REQUEST['action'] == 'edit') ? true : false;
+
+  $GLOBALS['bikePrice'] = [
+    'Half day',
+    'Full day',
+    'Day',
+  ];
+
+
+  function insertNestedInfo( $table, $info , $vendorId, $parentIds = []){
+
+    global $wpdb;
+    global $edit;
+
+    if( class_exists('gfBikesCustomization') ){
+      $obj = new gfBikesCustomization();
+    }
+
+    foreach($info as $value){
+      if(empty($value['name'])){
+        $nameError       = $obj->requiredMessage("error","Please Select at least one Checkbox");
+        $validationError = true;
+        continue;
+      }
+
+      $commonRow = [
+        'name'      => $value['name'],
+        'value'     => '1',
+        'vendor_id' => $vendorId
+      ];
+
+      global $bikePrice;
+
+      $parentIdArray = [];
+
+      foreach($bikePrice as $key=>$bikeLabel){
+        $row              = $commonRow;
+        $row['price']     = $value['price'][$key];
+        $row['frequency'] = $bikeLabel;
+
+        if(isset( $parentIds[$bikeLabel])){
+          $row['parent_id'] = $parentIds[$bikeLabel];
+        }
+
+        $insertQuery = $wpdb->insert($table , $row , array('%s', '%f', '%d', '%d'));
+
+        $parentIdArray[$bikeLabel] = $wpdb->insert_id;
+      }
+
+      $rowId = $wpdb->insert_id;
+
+      if($insertQuery === false){
+        echo ' We got errror ';
+        continue;
+      }
+
+      if(empty($value['children'])){
+        continue;
+      }
+
+      // Is Child Present
+      insertNestedInfo($table, $value['children'], $vendorId, $parentIdArray);
+    }
+  }
+
+  try{
+    $message = "";
+    global $edit;
+    if(isset($_REQUEST['add'])){
+
+      if( class_exists('gfBikesCustomization') ){
+        $obj = new gfBikesCustomization();
+      }
+
+      // Inserting Database for Vendor Name
+
+      //Start Here
+      $vendor  = $_REQUEST['name'];
+
+      $validationError   = false;
+      if(empty($vendor)){
+        $nameError       = $obj->requiredMessage("error","Please Enter vendor Name And Select At least One Checkbox");
+        $validationError = true;
+      }
+
+      if($validationError === false){
+        $tableVendor  = $wpdb->prefix . "vendor";
+
+        global $edit;
+
+
+        if($edit == true){ // If Edit File Call
+
+          $vendorId     = $_GET['post'];
+          $tableVendor  = $wpdb->prefix . "vendor";
+          $a1           = $_REQUEST['bikeName'];
+          $a2           = $_REQUEST['addOnName'];
+          $combineArray = array_merge($a1,$a2);
+          $insertVendor = $wpdb->update($tableVendor , array('name' => $vendor) ,array( 'id' => $vendorId ));
+          $tableName    = $wpdb->prefix . "bike";
+
+          ### delete previous records
+          $vendorStatus =  $wpdb->delete($tableName , ['vendor_id' => $vendorId] , array('%d'));
+          if( $vendorStatus !== false ){
+            insertNestedInfo($tableName,  $combineArray, $vendorId );
+            $message = $obj->requiredMessage("updated","Data Updated Successfully");
+          }
+          else{
+            $message = $obj->requiredMessage("error","Data Not Updated");
+          }
+        }
+        else{ // If Add File Call
+
+          $tableName    = $wpdb->prefix . "bike";
+          $a1           = $_REQUEST['bikeName'];
+          $a2           = $_REQUEST['addOnName'];
+          $combineArray = array_merge($a1,$a2);
+
+          $fetch   = $wpdb->get_results("SELECT * FROM `$tableVendor` WHERE `name` = '$vendor' LIMIT 1");
+          $data    = count($fetch);
+          if($data >= 1){
+            $message       = $obj->requiredMessage("error", "Data Not Inserted Because Vendor Name $vendor Already Exist");
+          }
+          else{
+            $insertVendor = $wpdb->insert($tableVendor , array('name' => $vendor) ,array('%s'));
+            $vendorId     = $wpdb->insert_id;
+            // End Here
+            insertNestedInfo($tableName,  $combineArray, $vendorId );
+            $message = $obj->requiredMessage("updated","Data Inserted Successfully");
+          }
+        }
+      } 
+    }    
+  }
+  catch(PDOException $e){
+    $message = $obj->requiredMessage("error","Your Data is not Inserted please contact the admin");
+  }
+
+/*
+* HTML START HERE ...
+*/
+
+
+  if($validationError === true){
+    echo $nameError;
+  }
+  echo $message;
+  global $edit;
+
+  function doNewColor(){
+    $color = dechex(rand(0x000000, 0xFFFFFF));
+    return $color;
+  }
+
+  $bikeArray = [
+    'road_bike' => [
+      'name' => 'Road Bike'
+    ],
+    'cruiser_bike' => [
+      'name' => 'Cruiser Bike'
+    ],
+
+    'mountain_bike' => [
+      'name' => 'Mountain Bike',
+      'children' => [
+
+        'downhill' => [
+          'name' => 'Downhill Bike',
+          'children' => [
+            'demo' => [
+              'name' => 'Demo Bike'
+            ],
+            'premium' => [
+              'name' => 'Premium Bike'
+            ],
+            'standard' => [
+              'name' => 'Standard Bike'
+            ],
+          ]
+        ],
+
+        'enduro' => [
+          'name' => 'Enduro Bike',
+          'children' => [
+            'demo' => [
+              'name' => 'Demo Bike'
+            ],
+            'premium' => [
+              'name' => 'Premium Bike'
+            ],
+            'standard' => [
+              'name' => 'Standard Bike'
+            ],
+          ]
+        ],
+
+        'trail' => [
+          'name' => 'Trail / All Mountain',
+          'children' => [
+            'demo' => [
+              'name' => 'Demo Bike'
+            ],
+            'premium' => [
+              'name' => 'Premium Bike'
+            ],
+            'standard' => [
+              'name' => 'Standard Bike'
+            ],
+          ]
+        ],
+
+        'cross' => [
+          'name' => 'Cross country Full Suspension',
+          'children' => [
+            'demo' => [
+              'name' => 'Demo Bike'
+            ],
+            'premium' => [
+              'name' => 'Premium Bike'
+            ],
+            'standard' => [
+              'name' => 'Standard Bike'
+            ],
+          ]
+        ],
+
+      ]
+    ],
+    'fat_bike' => [
+      'name' => 'Fat Bike'
+    ],
+    'electric_bike' => [
+      'name' => 'Electric Bike'
+    ],
+    'hybrid_bike' => [
+      'name' => 'Hybrid Bike'
+    ],
+
+  ];
+
+  $addOnArray = [
+
+    'add_on_damage' => [
+      'name' => 'Damage Protection'
+    ],
+    'add_on_pedals' => [
+      'name' => 'Pedals'
+    ],
+
+    'add_on_helmet' => [
+      'name' => 'Helmet'
+    ],
+    'add_on_lock' => [
+      'name' => 'Bike Lock'
+    ],
+    'add_on_rack' => [
+      'name' => 'Bike Rack'
+    ],
+    'add_on_leg' => [
+      'name' => 'Leg Armor'
+    ],
+    'add_on_neck' => [
+      'name' => 'Neck Armor'
+    ],
+    'add_on_elbow' => [
+      'name' => 'Elbow Armor'
+    ],
+    'add_on_downhillhelmet' => [
+      'name' => 'Downhill Helmet'
+    ],
+
+  ];
+
+
+  /*
+   * Function Name :
+   *
+   * Recursive Function
+   *
+   */
+  function outPutChildren($bikeArray, $parentName, $init = 0 ){
+
+    if($init !== 0){
+      echo '<div class="inner-children-info">';
+    }
+
+    foreach( $bikeArray as $bikeName => $bikeInfo){
+      echo '<div class="single-level-bike-info">';
+      $name = "{$parentName}[{$bikeName}]";
+      renderCheckboxWithInput($name , $bikeInfo['name'], $init );
+
+      if(!empty( $bikeInfo['children'] )){
+        outPutChildren( $bikeInfo['children'], "{$name}[children]", $init + 1 );
+      }
+      echo '</div>';
+    }
+    if($init !== 0){
+      echo '</div>';
+    }
+  }
+?>
+<style type="text/css">
+  
+  .single-level-bike-info > table .price-field,
+  .inner-children-info{
+    display: none;
+  }
+  
+  .show-children > table .price-field{
+    display: inline;
+  }
+  
+  .show-children > .inner-children-info{
+    display: block;
+  }
+  label.price-field {
+    padding: 0px 0px 0px 54px;
+  }
+  table{
+    width: 100%;
+  }
+  .form-table td{
+    margin:0;
+    padding:0;
+  }
+  .single-level-bike-info table td{
+    width:200px;
+  }
+  .single-level-bike-info table{
+    box-shadow:0px 2px 0px 0px #c7c7c7;
+    padding:10px 0;
+  }
+</style>
+<?php
+
+  function renderCheckboxWithInput( $name, $label, $init ){
+
+    global $edit;
+
+    $checkboxName = $name . '[name]';
+    $inputName    = $name . '[price][]';
+
+    $paddingCSS   = ($init !== 0 ) ? "padding-left:".($init*30)."px" : "";
+    ?>
+    <table>
+      <?php
+        $explodedName =  explode("[", $checkboxName);
+
+        $refinedKey   = array_map(function($item){
+          return trim( $item, "]" );
+        },$explodedName);
+
+        $checkboxValue = "";
+        if($edit == true){
+
+          global $wpdb;
+
+          $bName        = $label;
+          $table        = $wpdb->prefix.'bike';
+          $vendorId     = $_GET['post'];
+
+          $defaultValue = "";
+
+          $sql =  "SELECT * FROM {$table} where name = '{$bName}' AND vendor_id = {$vendorId}";
+
+          $row = $wpdb->get_row( $sql );
+
+          if(!empty($row) && !empty($row->name)){
+            $checkboxValue = "1";
+          } 
+
+          if(!empty($_POST)){
+            $intialArray   = $_POST;
+            foreach($refinedKey as $singleKey){
+              $intialArray =  $intialArray[$singleKey];
+            }
+            $checkboxValue = $intialArray;
+          }
+        }
+        else{
+          $intialArray   = $_POST;
+          foreach($refinedKey as $singleKey){
+            $intialArray =  $intialArray[$singleKey];
+          }
+          $checkboxValue = $intialArray;
+        }
+      ?> 
+      <td style="<?php echo $paddingCSS; ?>">
+        <label for="<?php echo $checkboxName; ?>">
+          <input 
+            type="checkbox" 
+            <?php  echo !(empty($checkboxValue)) ? 'checked="checked"' : ''; ?>
+            name="<?php echo $checkboxName; ?>"
+            id="<?php echo $checkboxName; ?>"
+            value="<?php echo $label;?>"
+            class="ids-nested-checkbox"
+          >
+            <?php echo $label;?>
+        </label>
+      </td>
+
+      <?php
+
+      global $bikePrice;
+
+      foreach($bikePrice as $key => $priceLabel){
+
+        if($edit == true){
+
+          $checkboxValue = $intialArray;
+
+          $defaultValue = "";
+
+          $bName = $label;
+
+          global $wpdb;
+          $table        = $wpdb->prefix.'bike';
+          $vendorId     = $_GET['post'];
+
+          $defaultValue = "";
+
+          $sql = "SELECT * FROM {$table} where name = '{$bName}' AND frequency ='{$priceLabel}' AND vendor_id = {$vendorId}";
+          $row = $wpdb->get_row( $sql  );
+
+          if(!empty($row) && !empty($row->price)){
+            $defaultValue = $row->price;
+          } 
+
+          if(!empty($_POST)){
+               
+            $explodedName =  explode("[",   trim( $inputName, "[]" ) );
+
+            $refinedKey   = array_map(function($item){
+              return trim( $item, "]" );
+            },$explodedName);
+
+            $checkboxValue = "";
+            $intialArray   = $_POST;
+            foreach($refinedKey as $singleKey){
+              $intialArray =  $intialArray[$singleKey];
+
+            }
+
+            if( !empty( $intialArray ) && !empty( $intialArray[$key]) ){
+              $defaultValue = $intialArray[$key];
+            }
+
+          }
+        }
+        else{
+
+          $explodedName =  explode("[",   trim( $inputName, "[]" ) );
+
+          $refinedKey = array_map(function($item){
+            return trim( $item, "]" );
+          },$explodedName);
+
+          $checkboxValue = "";
+          $intialArray = $_POST;
+          foreach($refinedKey as $singleKey){
+            $intialArray =  $intialArray[$singleKey];
+
+          }
+
+          $checkboxValue = $intialArray;
+
+          $defaultValue = "";
+
+          if( !empty( $intialArray ) && !empty( $intialArray[$key]) ){
+            $defaultValue = $intialArray[$key];
+          }
+        }
+
+        ?>
+        <td style="width:20%;">
+          <label class="price-field" for="<?php echo $inputName . $key; ?>">
+            <?php # echo $priceLabel; ?>
+            <input
+              name="<?php echo $inputName; ?>"
+              type="number" 
+              id="<?php echo $inputName . $key; ?>"
+              value="<?php echo $defaultValue; ?>"
+              style="width:100px; max-width: 100%;"
+              class="small-text" 
+              placeholder="<?php echo $priceLabel; ?>"
+            >
+          </label>
+        </td>
+    <?php
+      }
+    ?>
+    </table>
+<?php
+  }
+
+  global $wpdb;
+  $table        = $wpdb->prefix.'vendor';
+  $vendorId     = $_GET['post'];
+  $vendorDetail = $wpdb->get_row( "SELECT * FROM {$table} where id = {$vendorId} " );
+?>
+  <div class="wrap">
+    <form method="post">
+      <table class="form-table">
+        <tbody>
+          <tr>
+            <?php
+              $this->generalAddField('name' , 'Vendor :' , empty($vendor) ? $vendorDetail->name : $vendor , 'Enter Vendor Name');
+            ?>
+          </tr>
+
+          <tr>
+            <th scope="row">Category :</th>
+            <td>
+              <fieldset>
+                <legend class="screen-reader-text">
+                  <span>Category :</span>
+                </legend>
+                <?php
+                  outPutChildren($bikeArray, 'bikeName', 0);
+                ?>
+              </fieldset>
+            </td>
+          </tr>
+
+          <tr>
+            <th scope="row">Add On :</th>
+            <td>
+              <fieldset>
+                <legend class="screen-reader-text">
+                  <span>Add On :</span>
+                </legend>
+                <?php
+                  outPutChildren($addOnArray, 'addOnName', 0);
+                ?>
+              </fieldset>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+        <?php $this->generalbutton('add' , 'Save Changes' ); ?>
+    </form> 
+  </div>
+<script type="text/javascript">
+  jQuery(document).ready(function($){
+
+    function manageCheckboxPrices(checkbox){
+      if(checkbox.is(':checked')){
+        checkbox.closest('.single-level-bike-info').addClass('show-children');
+      }else{
+        checkbox.closest('.single-level-bike-info').removeClass('show-children');
+      }
+    }
+
+    $('.ids-nested-checkbox').click(function(){
+      manageCheckboxPrices($(this));
+    });
+
+    $('.ids-nested-checkbox').each(function(){
+      manageCheckboxPrices($(this));
+    });
+  });
+</script>
