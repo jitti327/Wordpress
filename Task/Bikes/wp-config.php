a2b4dceb59f4681d744e6ace86e5ba9b247d3dbb
incredible <jitti327@gmail.com>
2019-02-07 17:18:50 +0530

    Upgrading wordpress

--- Task/Bikes/wp-config.php
+++ Task/Bikes/wp-config.php
@@ -17,10 +17,13 @@
  *
  * @package WordPress
  */
-
+ini_set('display_startup_errors', 1);
+ini_set('display_errors', 1);
+error_reporting(-1);
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define('DB_NAME', 'bikes');
+
 
 /** MySQL database username */
 define('DB_USER', 'root');
@@ -78,7 +81,7 @@ $table_prefix  = 'wp_';
  *
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
-define('WP_DEBUG', false);
+define('WP_DEBUG', true);
 
 /* That's all, stop editing! Happy blogging. */
 
