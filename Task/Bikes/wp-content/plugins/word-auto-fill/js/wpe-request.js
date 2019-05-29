d88e7faabf17e6dcc9170504e485d0db6e9456a8
incredible <jitti327@gmail.com>
2019-05-29 11:19:27 +0530

    Delete the bikes task from the repository

--- Task/Bikes/wp-content/plugins/word-auto-fill/js/wpe-request.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(
-                       function()
-                       {
-                            alert("Your request has been sent successfully. You will be contacted soon, once your password is reset.");
-                       }
-                      );
